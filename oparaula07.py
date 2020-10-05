b069d3f41f44bd9e65ebbec550ceb2488bd0297f
jessica almeida de oliveira <jessicajflalmeida@gmail.com>
2020-10-05 14:23:00 -0300

    mais um aquivos pythom

---
+++ oparaula07.py
@@ -0,0 +1,9 @@
+n1=int(input("digite um valo"))
+n2=int(input("digite outro e valor")) 
+s=n1+n2
+m=n1*n2
+d=n1/n2
+di=n1//n2
+e=n1**n2
+print("a soma e {} o produto e{} a divção e {}".format(s,m,d))
+print("divião inteira{} poteciação{}".format(di,e)) 
